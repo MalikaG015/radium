0151ddfc35f1fe55baa2be5428e5a05b5f211116
MalikaG015 <93572684+MalikaG015@users.noreply.github.com>
2021-11-24 20:24:10 +0530

    Update appMiddleware.js

--- src/middlewares/appMiddleware.js
+++ src/middlewares/appMiddleware.js
@@ -3,14 +3,17 @@ const tokenCheck = function(req, res, next){
     let token=req.headers['x-auth-token']
     let validToken=jwt.verify(token,'radium')
     if(validToken){
-     req.validToken=validToken
-     next()
-    }
+        if(validToken._id==req.params.userId){
+          next()
+
+        }
+        else{
+            res.send({status:false, msg:"not authorized"}) 
+        }
+     }
     else{
         res.send({status:false, msg:"Invalid token"})
-
-    }
-
+}
 }
 
 module.exports.tokenCheck = tokenCheck
