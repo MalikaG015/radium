73665c796fab2374d8c46facbb60e1da2d382390
MalikaG015 <malikagulati015@gmail.com>
2021-12-03 17:51:41 +0530

    project-2

---
+++ src/models/collegeModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique:true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    logoLink: {
+        
+    }, 
+    isDeleted: {
+        type: Boolean,
+        default: false
+        }
+    }, { timestamps: true })
+
+module.exports = mongoose.model('CollegeDb', collegeSchema)
+
